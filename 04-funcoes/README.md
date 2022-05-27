d8dd2b2c9986352dae2c6ff485c324d7e79070fc
Rafael Aguiar <rafaelpaguiar@gmail.com>
2022-05-27 14:33:15 -0300

    Functions

---
+++ 04-funcoes/README.md
@@ -0,0 +1,3 @@
+* Funções no Go são tipos de variáveis que podem ser atribuídas, retornadas como retornos de outras funcões ou passadas como parâmentros;
+* As funções no Go podem ter mais de um retorno;
+* Para ignorar o valor de uma função que retorna mais de um valor pode-se utilizar **_** como nome da variável que recebe o retorno a ser ignorado;
---
+++ 04-funcoes/funcoes.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func somar(n1 int8, n2 int8) int8 {
+	return n1 + n2
+}
+
+//Função com retornos múltiplos
+func calculosMatematicos(n1, n2 int8) (int8, int8) {
+	soma := n1 + n2
+	subtracao := n1 - n2
+	return soma, subtracao
+
+}
+
+func main() {
+	somar := somar(20, 30)
+	fmt.Println(somar)
+
+	var f = func(txt string) string {
+		fmt.Println(txt)
+		return txt
+	}
+
+	f("Texto da função f")
+
+	resultadoSoma1, resultadoSubtrcao1 := calculosMatematicos(10, 20)
+	fmt.Println(resultadoSoma1, resultadoSubtrcao1)
+
+	_, resultadoSubtrcao2 := calculosMatematicos(50, 40)
+	fmt.Println(resultadoSubtrcao2)
+
+}
